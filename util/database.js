03cf652dc18ac6a58ab6e1f00041b06bddfa870f
Alex Mgw <egjuice99@gmail.com>
2023-10-13 21:54:33 +0300

    database now reconfigured for .env for enchanced database security

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-
-const DB_HOST = "localhost";
-const DB_USER = "root";
-const DB_PASSWORD = "admin";
-const DB_DATABASE = "social_media";
-const DB_PORT = 3306;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 
 const pool = mysql.createPool({
   connectionLimit: 100,
